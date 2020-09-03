238f3ee920274dd908c9701271fdbc4f959f592c
Christopher Szatmary <cs@christopherszatmary.com>
2020-09-03 17:02:02 -0400

    Add reCAPTCHA to protect send email from spam

--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -1,5 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import MailService from "@sendgrid/mail";
+import nodeFetch from "node-fetch";
+import { Result, env, log } from "@cszatma/node-stdlib";
+
+const fetch = Result.resultifyPromise(nodeFetch);
+log.std.formatter = new log.TextFormatter({ disableTimestamp: true });
 
 async function emailHandler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
   if (req.method !== "POST") {
@@ -7,21 +12,31 @@ async function emailHandler(req: NextApiRequest, res: NextApiResponse): Promise<
     return;
   }
 
-  const sendgridApiKey = process.env.SENDGRID_API_KEY;
-  if (!sendgridApiKey) {
+  const requiredEnvVars = ["SENDGRID_API_KEY", "RECAPTCHA_SECRET_KEY"];
+  const missingEnvVars: string[] = [];
+  for (const envVar of requiredEnvVars) {
+    if (!env.isEnvSet(envVar)) {
+      missingEnvVars.push(envVar);
+    }
+  }
+
+  if (missingEnvVars.length > 0) {
+    log.std.error("Missing required env var(s)", {
+      envVars: missingEnvVars,
+    });
     res.status(500).json({
       error: {
-        code: "err_missing_env_var",
-        message: `Missing value for env var SENDGRID_API_KEY`,
+        code: "err_missing_env_vars",
+        message: `Missing the required env var(s): ${missingEnvVars.join(", ")}`,
       },
     });
     return;
   }
 
-  MailService.setApiKey(sendgridApiKey);
+  MailService.setApiKey(env.getEnv("SENDGRID_API_KEY"));
 
   // Verify all fields are present in the body
-  const requiredFields = ["name", "email", "subject", "message"];
+  const requiredFields = ["name", "email", "subject", "message", "recaptchaKey"];
   const missingFields: string[] = [];
   for (const field of requiredFields) {
     if (typeof req.body[field] !== "string") {
@@ -34,6 +49,43 @@ async function emailHandler(req: NextApiRequest, res: NextApiResponse): Promise<
       error: {
         code: "err_missing_body_fields",
         message: `Missing the required field(s) from the request body: ${missingFields.join(", ")}`,
+      },
+    });
+    return;
+  }
+
+  // Verify reCAPTCHA
+  const result = await fetch("https://www.google.com/recaptcha/api/siteverify", {
+    method: "post",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+    },
+    body: `secret=${env.getEnv("RECAPTCHA_SECRET_KEY")}&response=${req.body.recaptchaKey}`,
+  });
+
+  if (result.isFailure()) {
+    log.std.error("Failed to verify reCAPTCHA", {
+      error: result.failure(),
+    });
+    res.status(500).json({
+      error: {
+        code: "err_recaptcha_verify_failed",
+        message: "Failed to verify reCAPTCHA",
+      },
+    });
+    return;
+  }
+
+  const verifyBody = await result.success().json();
+  if (!verifyBody.success) {
+    log.std.warn("reCAPTCHA verification failed", {
+      result: verifyBody,
+    });
+    res.status(400).json({
+      error: {
+        code: "err_invalid_recaptcha",
+        message: "Invalid reCAPTCHA value provided",
       },
     });
     return;
@@ -59,13 +111,16 @@ async function emailHandler(req: NextApiRequest, res: NextApiResponse): Promise<
   } catch (err) {
     // TODO figure out better error handling
     // need to figure out what err actually is
-    console.error(err);
+    log.std.error("Failed to send email", {
+      error: err,
+    });
     res.status(500).json({
       error: {
         code: "err_send_email",
         message: "Failed to send email",
       },
     });
+    return;
   }
 
   res.status(200).json({});
