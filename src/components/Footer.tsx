6413193ba694f519965d5d5725de6f9ab777b2f0
Christopher Szatmary <cs@christopherszatmary.com>
2020-09-05 16:26:26 -0400

    Add github and linkedin to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,23 +3,40 @@ import { Container, Row, Col } from "reactstrap";
 
 import styles from "@/styles/components/Footer.module.scss";
 import { classNames } from "@/utils/mod";
+import { LinkBlank } from "./LinkBlank";
 import { LogoImg } from "./LogoImg";
 
-export const Footer: FunctionComponent = () => (
-  <footer className={classNames(styles.footer, "white-text nunito")}>
-    <Container fluid>
-      <Row className="align-items-center">
-        <Col className="text-center py-3" xs="12" md="5">
-          <span className={styles.span}>Developed by Christopher Szatmary</span>
-        </Col>
-        <Col className="text-center py-2" xs="12" md="2">
-          <LogoImg version="mixed" />
-        </Col>
-        <Col className="text-center py-3" xs="12" md="5">
-          All rights reserved.&nbsp;
-          <span className={styles.span}>Copyright © 2017-2020 Christopher Szatmary.</span>
-        </Col>
-      </Row>
-    </Container>
-  </footer>
-);
+export const Footer: FunctionComponent = () => {
+  return (
+    <footer className={styles.footer}>
+      <Container fluid>
+        <Row className="d-flex align-items-center">
+          <Col className={classNames("py-3", styles.leftSection)} xs="12" md="5">
+            <span className={styles.leftSpan}>Developed by Christopher Szatmary</span>
+            <div className={styles.linkGroup}>
+              <LinkBlank
+                href="https://github.com/cszatma"
+                className={classNames("mx-2", styles.socialLink)}
+              >
+                <i className={classNames("devicon-github-plain", styles.socialIcon)} />
+              </LinkBlank>
+              <LinkBlank
+                href="https://www.linkedin.com/in/christopherszatmary/"
+                className={classNames("mx-2", styles.socialLink)}
+              >
+                <i className={classNames("devicon-linkedin-plain", styles.socialIcon)} />
+              </LinkBlank>
+            </div>
+          </Col>
+          <Col className="text-center py-2" xs="12" md="2">
+            <LogoImg version="mixed" />
+          </Col>
+          <Col className="text-center py-3" xs="12" md="5">
+            All rights reserved.&nbsp;
+            <span className={styles.rightSpan}>Copyright © 2017-2020 Christopher Szatmary.</span>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
