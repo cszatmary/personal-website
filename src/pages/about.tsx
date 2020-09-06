82d6e0743a22faadc49f476019aee58c701f683e
Christopher Szatmary <cs@christopherszatmary.com>
2020-09-05 23:58:25 -0400

    Redo a lot of the about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,23 +1,11 @@
 import React, { FunctionComponent } from "react";
+import Link from "next/link";
 import { Container, Row, Col } from "reactstrap";
 
 import { Animation } from "@/components/Animation";
 import { LinkBlank } from "@/components/LinkBlank";
 import styles from "@/styles/pages/about.module.scss";
-import { classNames } from "@/utils/mod";
-
-const socialItems = [
-  {
-    name: "github",
-    icon: "devicon-github-plain",
-    link: "https://github.com/cszatma",
-  },
-  {
-    name: "linkedin",
-    icon: "devicon-linkedin-plain",
-    link: "https://www.linkedin.com/in/christopherszatmary/",
-  },
-];
+import { classNames, constants } from "@/utils/mod";
 
 const skillItems = [
   { name: "Go", icon: "devicon-go-plain" },
@@ -29,15 +17,9 @@ const skillItems = [
 ];
 
 const AboutPage: FunctionComponent = () => {
-  const links = socialItems.map((item) => (
-    <LinkBlank href={item.link} className={classNames("mx-3", styles.socialLink)} key={item.name}>
-      <i className={classNames("white-text fa-3x", item.icon)} />
-    </LinkBlank>
-  ));
-
   const skills = skillItems.map((item) => (
     <Col className="my-1" sm="6" lg="4" key={item.name}>
-      <Row className="text-center align-items-center">
+      <Row className="align-items-center">
         <Col className="text-right" xs="6">
           <Animation animation="shake" hover>
             <i className={classNames(item.icon, styles.skillIcon)} />
@@ -65,7 +47,7 @@ const AboutPage: FunctionComponent = () => {
             <Row className="py-2">
               <p className="px-3">
                 Hi I&apos;m Chris, a software developer with a passion for automation. I love
-                building to tools to automate common tasks and solve interesting problems. I am
+                building tools to automate common tasks and solve interesting problems. I am
                 currently enrolled at Ryerson University majoring in Computer Science and minoring
                 in Mathematics.
               </p>
@@ -74,49 +56,56 @@ const AboutPage: FunctionComponent = () => {
                 for the Toronto Maple Leafs for better or for worse.
               </p>
             </Row>
-            <Row className="py-2 align-self-center">{links}</Row>
           </Col>
         </Row>
       </Container>
       <Container className="py-3">
         <h2 className="my-2">What I do</h2>
-        <Row className="mt-3">{skills}</Row>
-        <Row className="justify-content-center my-3">
-          <Col className="py-2">
-            <p>
-              I have experience with a lot of languages, libraries, and tools. Above are the main
-              ones I currently use. I have experience with both fullstack and iOS development.
-            </p>
-            <p>
-              I have recently fallen in love with the roles of developer acceleration and production
-              engineering.
-            </p>
-            <p>
-              This involves:
-              <ul>
-                <li>
-                  Eliminating toil for developers to make it as easy as possible for them to do
-                  their jobs.
-                </li>
-                <li>Create robust production systems that are easy to understand and maintain.</li>
-              </ul>
-            </p>
-            <p>
-              How are these goals achieved?
-              <ul>
-                <li>
-                  Create tools to make it easy for developers to manage their local environment.
-                </li>
-                <li>Create CI pipelines to automate various tasks.</li>
-                <li>Create CD pipelines to easily deploy applications to production.</li>
-                <li>
-                  Implement great observability to easy know the state of applications in
-                  production.
-                </li>
-              </ul>
-            </p>
-          </Col>
-        </Row>
+        <p>
+          I have experience with a lot of languages, libraries, and tools. Here are the main ones I
+          currently use:
+        </p>
+        <Row className="my-3">{skills}</Row>
+        <p>
+          I have recently fallen in love with the roles of developer acceleration and production
+          engineering.
+        </p>
+      </Container>
+      <Container className="py-3">
+        <h2 className="my-2">What even is Developer Acceleration?</h2>
+        <p>
+          Developer Accelerate (Dev Accel) is all about making it easy as possible for developers to
+          deliver value to customers. I build tools to make local development as easy and painless
+          as possible. I also set up CI/CD processes to make it easy and safe to ship code to
+          production and roll it back when something goes wrong.
+        </p>
+        <p>
+          I&apos;ve always enjoyed the area of developer efficiency and Dev Accel has been a perfect
+          fit for me. Any time I find something tedious that I constantly have to do that slows me
+          down, or I find something that is way harder than it should be, I find a way to automate
+          it.
+        </p>
+      </Container>
+      <Container className="py-3">
+        <h2 className="my-2">Want to know more?</h2>
+        <p>
+          <span>Check out some of the </span>
+          <Link href="/projects">
+            <a>projects</a>
+          </Link>
+          <span> I&apos;ve done, or </span>
+          <Link href="/contact">
+            <a>contact me</a>
+          </Link>
+          <span>.</span>
+        </p>
+        <p>
+          <span>Also check out my </span>
+          <LinkBlank href={constants.github}>GitHub</LinkBlank>
+          <span> and </span>
+          <LinkBlank href={constants.linkedin}>LinkedIn</LinkBlank>
+          <span>.</span>
+        </p>
       </Container>
     </div>
   );
